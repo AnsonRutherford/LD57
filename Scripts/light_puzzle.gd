df28a5a0229d875afd897f43c7c5b5955432e808
Anson Rutherford <>
2025-04-05 00:37:01 -0400

    light puzzle can be solved

--- Scripts/light_puzzle.gd
+++ Scripts/light_puzzle.gd
@@ -1,1 +1,18 @@
+class_name LightPuzzle
 extends Node
+
+static var instance: LightPuzzle = null
+static var pillars: Dictionary[MirrorPillar.PILLAR_INDEX, MirrorPillar] = {}
+
+@export var solution: Array[int]
+
+func _ready() -> void:
+	instance = self
+
+static func register_pillar(pillar: MirrorPillar) -> void:
+	pillars[pillar.pillar_index] = pillar
+
+static func check_solution() -> void:
+	var pillar1 = pillars[MirrorPillar.PILLAR_INDEX.PILLAR1]
+	if pillar1.has_mirror and pillar1.rotate_state == 3:
+		Globals.PUZZLE_SOLVED.emit(Globals.PUZZLE.LIGHT) 
--- Scripts/mirror_pillar.gd
+++ Scripts/mirror_pillar.gd
@@ -1,7 +1,10 @@
 class_name MirrorPillar
 extends Node
 
+enum PILLAR_INDEX { PILLAR1, PILLAR2, PILLAR3 }
+
 @export var has_mirror := false
+@export var pillar_index: PILLAR_INDEX
 
 @onready var mirror := $Mirror
 @onready var interact_collision := $"Interact Collision"
@@ -13,6 +16,7 @@ var rotate_state: int = 0
 	
 func _ready() -> void:
 	add_to_group("Interactable")
+	LightPuzzle.register_pillar(self)
 	if has_mirror:
 		gain_mirror()
 	else:
@@ -27,15 +31,19 @@ func gain_mirror():
 	has_mirror = true
 	mirror.visible = true
 	interact_collision.position = Vector3.UP / 2
+	rotate_state = 0
+	create_tween().tween_property(mirror, "rotation", rotations[rotate_state], 0)
 
 func interact() -> void:
 	rotate_state = (rotate_state + 1) % len(rotations)
 	create_tween().tween_property(mirror, "rotation", rotations[rotate_state], rotation_duration)
+	LightPuzzle.check_solution()
 	
 func pickup(player: Player) -> void:
 	if !has_mirror and player.held_item == Player.HELD_ITEM.MIRROR:
 		gain_mirror()
 		player.lose_held_item()
+		LightPuzzle.check_solution()
 	elif has_mirror and player.held_item == Player.HELD_ITEM.NONE:
 		lose_mirror()
 		player.handle_hold_item(Player.HELD_ITEM.MIRROR)
