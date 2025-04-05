9ec53a90c0dfa4a5dc3a6b6686cb7b2633fa81a7
Anson Rutherford <>
2025-04-05 11:56:29 -0400

    light puzzle beam + graphics

--- Scripts/light_puzzle.gd
+++ Scripts/light_puzzle.gd
@@ -6,6 +6,8 @@ static var pillars: Dictionary[MirrorPillar.PILLAR_INDEX, MirrorPillar] = {}
 
 @export var solution: Array[int]
 
+@onready var light_beam = $LightBeam
+
 func _ready() -> void:
 	instance = self
 
@@ -14,5 +16,13 @@ static func register_pillar(pillar: MirrorPillar) -> void:
 
 static func check_solution() -> void:
 	var pillar1 = pillars[MirrorPillar.PILLAR_INDEX.PILLAR1]
-	if pillar1.has_mirror and pillar1.rotate_state == 3:
-		Globals.PUZZLE_SOLVED.emit(Globals.PUZZLE.LIGHT)
+	if !pillar1.has_mirror:
+		instance.light_beam.scale.z = 28
+	else:
+		instance.light_beam.scale.z = 7
+		pillar1.light_beam.scale.z = 0.1
+		if pillar1.rotate_state == 1:
+			pillar1.light_beam.scale.z = 3
+		if pillar1.rotate_state == 3:
+			pillar1.light_beam.scale.z = 3
+			Globals.PUZZLE_SOLVED.emit(Globals.PUZZLE.LIGHT)
--- Scripts/mirror_pillar.gd
+++ Scripts/mirror_pillar.gd
@@ -7,14 +7,20 @@ enum PILLAR_INDEX { PILLAR1, PILLAR2, PILLAR3 }
 @export var pillar_index: PILLAR_INDEX
 
 @onready var mirror := $Mirror
+@onready var light_beam := %LightBeam
 @onready var interact_collision := $"Interact Collision"
 
 const rotation_duration := 0.1
 
 var rotate_state: int = 0
-@export var rotations: Array[Vector3]
+@export var angles: Array[float]
+var rad_angles: Array[float]
 	
 func _ready() -> void:
+	rad_angles = []
+	for angle in angles:
+		rad_angles.append(deg_to_rad(angle))
+		
 	add_to_group("Interactable")
 	LightPuzzle.register_pillar(self)
 	if has_mirror:
@@ -32,11 +38,22 @@ func gain_mirror():
 	mirror.visible = true
 	interact_collision.position = Vector3.UP / 2
 	rotate_state = 0
-	create_tween().tween_property(mirror, "rotation", rotations[rotate_state], 0)
+	var rotation = Vector3(0, rad_angles[rotate_state], 0)
+	create_tween().tween_property(mirror, "rotation", rotation, 0)
 
 func interact() -> void:
-	rotate_state = (rotate_state + 1) % len(rotations)
-	create_tween().tween_property(mirror, "rotation", rotations[rotate_state], rotation_duration)
+	var current_angle = rad_angles[rotate_state]
+	rotate_state = (rotate_state + 1) % len(angles)
+	var next_angle = rad_angles[rotate_state]
+	if next_angle < current_angle:
+		next_angle += TAU
+	
+	var tween = create_tween()
+	var rotation = Vector3(0, next_angle, 0)
+	tween.tween_property(mirror, "rotation", rotation, rotation_duration)
+	rotation = Vector3(0, rad_angles[rotate_state], 0)
+	tween.tween_property(mirror, "rotation", rotation, 0)
+	
 	LightPuzzle.check_solution()
 	
 func pickup(player: Player) -> void:
@@ -47,3 +64,4 @@ func pickup(player: Player) -> void:
 	elif has_mirror and player.held_item == Player.HELD_ITEM.NONE:
 		lose_mirror()
 		player.handle_hold_item(Player.HELD_ITEM.MIRROR)
+		LightPuzzle.check_solution()
