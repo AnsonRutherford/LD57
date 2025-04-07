f77d0461e481e38b580338fabda3ef02791c6613
Anson Rutherford <>
2025-04-07 15:51:52 -0400

    asset dump

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,24 +1,30 @@
 extends Node3D
 
-@onready var mesh = $MeshInstance3D
+@onready var visual = $Visual
 @onready var body = $StaticBody3D
+@onready var eye_contact = $EyeContact
 
 @export var lines: Array[String]
 var line_index = 0
 
 func _ready() -> void:
 	Globals.CURSE_LEVEL_CHANGED.connect(_on_curse_level_changed)
-	mesh.visible = false
+	visual.visible = false
 	body.process_mode = PROCESS_MODE_DISABLED
+	
+func _process(_delta) -> void:
+	var pos = Globals.player.global_position
+	pos.y = global_position.y
+	# look_at(pos)
 
 func interact() -> void:
-	Globals.start_dialogue(self, lines[0])
+	Globals.start_dialogue(eye_contact, lines[0])
 	line_index += 1
 	Globals.DIALOGUE_NEXT.connect(next_line)
 	
 func next_line() -> void:
 	if line_index < len(lines):
-		Globals.continue_dialogue(self, lines[line_index])
+		Globals.continue_dialogue(eye_contact, lines[line_index])
 		line_index += 1
 	else:
 		Globals.DIALOGUE_NEXT.disconnect(next_line)
@@ -26,8 +32,8 @@ func next_line() -> void:
 	
 func _on_curse_level_changed() -> void:
 	if Globals.curse_level == Globals.CURSE.NOT_CURSED:
-		mesh.visible = false
+		visual.visible = false
 		body.process_mode = PROCESS_MODE_DISABLED
 	else:
-		mesh.visible = true
+		visual.visible = true
 		body.process_mode = PROCESS_MODE_INHERIT
