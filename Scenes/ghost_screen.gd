b33488ec8d8b2778f1e5f0c7b1133150f85bd625
Adam Giles <adamag@cox.net>
2025-04-07 20:48:38 -0400

    Yeah

--- Scenes/ghost_screen.gd
+++ Scenes/ghost_screen.gd
@@ -4,14 +4,17 @@ var time_left: float = 3.0
 
 @onready var sprite: Sprite2D = $Sprite2D
 
-const base_pos := Vector2(600, 300)
+var base_pos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	base_pos = get_viewport_rect().size / 2
 	sprite.modulate = Color(1, 1, 1, 0)
 	var tween = create_tween()
 	tween.tween_property(sprite, "modulate", Color(1, 1, 1, 1), 1.5)
 	tween.tween_property(sprite, "modulate", Color(1, 1, 1, 0), 1.5)
+	
+	$AudioStreamPlayer.play(.7)
 	
 	#var skew_tween = create_tween()
 	#skew_tween.tween_property(sprite, "skew", -.1, 1)
@@ -29,4 +32,6 @@ func _process(delta: float) -> void:
 	time_left -= delta
 	
 	if time_left < 0:
+		visible = false
+		await get_tree().create_timer(2.0).timeout
 		queue_free()
